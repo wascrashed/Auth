321ea2231d3af0b3082790ccf677be39386624f2
wascrashed <rebuxtv@gmail.com>
2023-05-03 12:36:45 +0500

    Auth

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullName');
+            $table->string('phoneNumber')->unique();
+            $table->integer('cdo_id');
+            $table->integer('role');
+            $table->timestamp('phoneNumber_verified_at')->nullable();
+            $table->string('password');
+            $table->string('address');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
